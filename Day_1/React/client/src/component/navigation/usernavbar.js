84b6f3cbfb46cfb90ae2d35d0089600e2e4c1ced
Shinigami <REEZANSHRESTHA15@GMAIL.COM>
2024-06-18 20:40:24 +0545

    react class
    react class

---
+++ Day_1/React/client/src/component/navigation/usernavbar.js
@@ -0,0 +1,20 @@
+import{useNavigate} from "react-router-dom"
+
+const UserNavBar =() =>{
+    const navigate = useNavigate()
+    return(
+        <>
+            <div onClick={()=> navigate("/")}>
+                home
+            </div>
+            <div onClick={()=> navigate("/services")}>
+                services
+            </div>
+            <div onClick={()=> navigate("/contact-us")}>
+                contact-us
+            </div>
+        
+        </>
+    )
+}
+export default UserNavBar;
---
+++ Day_1/React/client/src/pages/01-homePage.js
@@ -0,0 +1,8 @@
+function Home(){
+
+    return (
+        <div> HOME PAGE</div>
+    )
+}
+
+export default Home
